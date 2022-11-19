433ff9ae6c7880ae598d3b92b0761238bcfcd699
NVMakarenko <taradator@ukr.net>
2022-11-19 20:54:45 +0200

    PR2 -7 chanfe attr in class Car to attr_reader

--- car.rb
+++ car.rb
@@ -3,17 +3,17 @@
 require 'date'
 
 class Car
-  attr_accessor :id, :make, :model, :year, :odometer, :price, :description, :date_added
+  attr_reader :id, :make, :model, :year, :odometer, :price, :description, :date_added
 
   def initialize(id, make, model, year, odometer, price, description, date_added)
-    self.id = id
-    self.make = make
-    self.model = model
-    self.year = year
-    self.odometer = odometer
-    self.price = price
-    self.description = description
-    self.date_added = Date.strptime(date_added, '%d/%m/%y')
+    @id = id
+    @make = make
+    @model = model
+    @year = year
+    @odometer = odometer
+    @price = price
+    @description = description
+    @date_added = Date.strptime(date_added, '%d/%m/%y')
   end
 
   def to_hash
